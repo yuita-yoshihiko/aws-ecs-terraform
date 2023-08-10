43498b44e80abad52555fb837815c57a0ea85f7d
yuita-yoshihiko <soup998211@gmail.com>
2023-08-11 08:57:14 +0900

    Create ECR repository

---
+++ ecr.tf
@@ -0,0 +1,32 @@
+resource "aws_ecr_repository" "ecr_repository" {
+  name                 = local.app
+  image_tag_mutability = "IMMUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "ecr_lifecycle_policy" {
+  repository = aws_ecr_repository.ecr_repository.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 30 images",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 30
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
