d4ed33fce0a2085dca604c39b63871e9064b4764
yuita-yoshihiko <soup998211@gmail.com>
2023-08-10 23:24:19 +0900

    Create network

--- main.tf
+++ main.tf
@@ -7,4 +7,15 @@ terraform {
     dynamodb_table = "aws-ecs-terraform-tfstate-locking"
     encrypt        = true
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
+
+ provider "aws" {
+   region = local.region
+ }
---
+++ network.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = local.name
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${local.region}a", "${local.region}c"]
+  public_subnets  = ["10.0.11.0/24", "10.0.12.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+
+  public_subnet_names  = ["Public Subnet 1a", "Public Subnet 1c"]
+  private_subnet_names = ["Private Subnet 1a", "Private Subnet 1c"]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  enable_nat_gateway = true
+}
